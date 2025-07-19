1bdb4bf89dec531ee0bf2e8589e88220b303dd92
sairevant <sairevanth.k@ashvfinance.com>
2025-07-19 19:48:31 +0530

    Resume Feedback page completed

---
+++ app/components/ATS.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ATS() {
+  return <div>ATS</div>;
+}
+
+export default ATS;
---
+++ app/components/ScoreBadge.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ScoreBadge() {
+  return (
+    <div>ScoreBadge</div>
+  )
+}
+
+export default ScoreBadge
